68e813b0cc01b5cbac7c8fb50d5637f106bf2b11
Jehovah Yii Zui Hon (Hovah) <31881261+hovahyii@users.noreply.github.com>
2020-09-20 23:05:13 +0800

    problem resolved

--- liet/src/components/NavigationBar.js
+++ liet/src/components/NavigationBar.js
@@ -26,7 +26,7 @@ export const NavigationBar = () => (
     <Styles>
         <Navbar expand="lg">
             <Navbar.Brand href="/">LIET</Navbar.Brand>
-            <Navbar.Toggle aria-constrols="basic-navbar-nav"/>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
                     <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
