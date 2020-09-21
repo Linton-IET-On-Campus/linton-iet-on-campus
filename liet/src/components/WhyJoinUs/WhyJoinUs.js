27fd25a1be85f484e7adaae77ef488a263069669
Jehovah Yii Zui Hon (Hovah) <31881261+hovahyii@users.noreply.github.com>
2020-09-22 03:57:39 +0800

    demo for REST API

--- liet/src/components/WhyJoinUs/WhyJoinUs.js
+++ liet/src/components/WhyJoinUs/WhyJoinUs.js
@@ -270,7 +270,7 @@ export const WhyJoinUs = () => {
   useEffect(() => {
     const fetchItems = async () => {
       const  result = await axios(
-          `https://raw.githubusercontent.com/Linton-IET-On-Campus/linton-iet-on-campus/master/server/data/why-join-us/index.json`
+          `https://linton-iet-on-campus.github.io/linton-iet-on-campus/backend/data/why-join-us/index.json`
         )
         console.log(result.data)
 
