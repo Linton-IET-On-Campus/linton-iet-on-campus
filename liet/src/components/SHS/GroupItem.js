0c3f0946ca5baa63deabac7e0055761c94b75a72
Jehovah Yii Zui Hon (Hovah) <31881261+hovahyii@users.noreply.github.com>
2020-09-23 05:53:01 +0800

    finish SHS

--- liet/src/components/SHS/GroupItem.js
+++ liet/src/components/SHS/GroupItem.js
@@ -6,23 +6,23 @@ import ListGroupItem from 'react-bootstrap/ListGroupItem';
 const GroupItem = ({ item }) => {
   return (
     <Card>
-    <Card.Img variant="top" src={item.img} width="274px" height="160px"  />
-    <Card.Body>
-      <Card.Title>{item.title}</Card.Title>
-      <Card.Subtitle className="mb-2 text-muted">{item.field}</Card.Subtitle>
-      <Card.Text>
-       {item.description}
-      </Card.Text>
-    </Card.Body>
-    <ListGroup className="list-group-flush">
-        <ListGroupItem><i class="fas fa-user"></i> {item.leader}</ListGroupItem>
-        <ListGroupItem><i class="fas fa-calendar-day"></i> {item.day}</ListGroupItem>
-        <ListGroupItem><i class="fas fa-clock"></i> {item.time}</ListGroupItem>
-    </ListGroup>
-    <Card.Body>
-        <Card.Link href={item.link}>Join Group</Card.Link>
-        <Card.Link href={item.meet}>Google Meet</Card.Link>
-    </Card.Body>
+        <Card.Img variant="top" src={item.img} width="274px" height="160px"  />
+        <Card.Body>
+            <Card.Title>{item.title}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">{item.field}</Card.Subtitle>
+            <Card.Text>
+                 {item.description}
+            </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+            <ListGroupItem><i class="fas fa-user"></i> {item.leader}</ListGroupItem>
+            <ListGroupItem><i class="fas fa-calendar-day"></i> {item.day}</ListGroupItem>
+            <ListGroupItem><i class="fas fa-clock"></i> {item.time}</ListGroupItem>
+        </ListGroup>
+        <Card.Body>
+            <Card.Link href={item.link}>Join Group</Card.Link>
+            <Card.Link href={item.meet}>Google Meet</Card.Link>
+        </Card.Body>
     </Card>
   )
 }
